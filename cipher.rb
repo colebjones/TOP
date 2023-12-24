93413404b49a1266fcc9de3334347784ee61b63e
Cole <24217604+colebjones@users.noreply.github.com>
2023-12-23 19:14:24 -0600

    Working Caesar cipher

---
+++ cipher.rb
@@ -0,0 +1,44 @@
+#hashmap of characters to ignore
+IGNORE = [" ", ".", "!", "?", "-", ";", "'", "`", "[", "]", "|", "\\", "<", 
+            ">", "=", "+", "@", "#", "$", "%", "^", "&", 
+            "*", "(", ")", "_", "{", "}", ":", "/", "~", 
+            "1", "2","3", "4", "5", "6", "7", "8", "9", "0"]
+            .map{|x| [x,true]}
+            .to_h
+
+def caesar_cipher(message, shift) 
+    #limit shift to be at most one wrap around through the alphabet via remainder of 26
+    shift > 26 ? shift = shift%26 : false
+
+    shifted_chars = message.split("").map do |letter|
+
+        #Skip non-alphabet characters
+        if IGNORE.has_key?(letter) 
+            letter = letter
+
+        #Handle the character shifting
+        else 
+            #Handle lower case wrap
+            if letter.ord < 123 and letter.ord > 96 
+                letter.ord + shift > 122 ? 
+                letter = letter.ord + shift - 26 : 
+                letter = letter.ord + shift  
+
+            #Handle upper case wrap
+            else 
+                letter.ord + shift > 89 ? 
+                letter = letter.ord + shift - 26 :
+                letter = letter.ord + shift      
+            end
+        end
+    end
+
+    #Convert the integers to their new characters, joins, and prints the new message
+    puts shifted_chars.map{|x| x = x.chr }.join("")
+end
+
+caesar_cipher("What a string!", 5)
+
+
+#az = 97|122
+#AZ = 65|90
